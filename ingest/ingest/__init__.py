09289a6e5f4ac76c7e8fc6e3e4676812b4a8a1d7
matthh9797 <matthewh@netextremity.com>
2023-05-12 08:35:14 +0700

    add fpl ingest instance

--- ingest/ingest/__init__.py
+++ ingest/ingest/__init__.py
@@ -3,5 +3,5 @@ from .base import Ingest
 ## @TODO for developer: Define child classes of base Ingest class 
 ## and override methods if required. Rename child class to match the source.
 
-class ChildA(Ingest):
+class IngestFpl(Ingest):
     pass
