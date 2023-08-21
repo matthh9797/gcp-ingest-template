5ac0d4c4ad9a3c73e944880d68ee608addcd760e
matthh9797jetty <matthew.henderson@jettytechnologies.com>
2023-08-21 14:31:40 +0100

    added multiple triggers to main

--- ingest/main.py
+++ ingest/main.py
@@ -1,12 +1,14 @@
 import os
 import logging
+import json
 from datetime import date
 from flask import Flask
 from flask import request, escape
 import google.cloud.logging
 
-from source_a import IngestA
-from utils import dict_from_yaml
+from pega import IngestPega
+from elevate import IngestElevate
+from utils import dict_from_yaml, verify_public_ip
 
 
 client = google.cloud.logging.Client()
@@ -20,27 +22,60 @@ logging.basicConfig(
 app = Flask(__name__)
 
 # Parameters
-@app.route("/source_a", methods=['POST'])
-def ingest_a():
+@app.route("/pega", methods=['POST'])
+def ingest_pega():
 
     try:
 
+        ip = verify_public_ip()
+        logging.info(ip)
+
         # request arguments
         json = request.get_json(force=True) # https://stackoverflow.com/questions/53216177/http-triggering-cloud-function-with-cloud-scheduler/60615210#60615210
+        download_strategy = escape(json['download_strategy']) if 'download_strategy' in json else 'winscp'
         increment_type = escape(json['increment_type']) if 'increment_type' in json else 'window'
         tables = escape(json['tables']) if 'tables' in json else 'all'
         env = escape(json['env']) if 'env' in json else 'prod'
         extract_date = escape(json['extract_date']) if 'extract_date' in json else date.today().strftime("%Y%m%d")
 
         overrides = {
+            'download_strategy': download_strategy,
             'increment_type': increment_type,
             'tables': tables,
             'env': env
         }
 
-        config = dict_from_yaml('source_a/config.yaml')
-        ingest_a = IngestA(config, extract_date, overrides)
-        ingest_a.run()
+        config = dict_from_yaml('pega/config.yaml')
+        ingest_pega = IngestPega(config, extract_date, overrides)
+        ingest_pega.run()
+        ok = 'Ingested successfully'
+        logging.info(ok)
+        return ok
+    except Exception as e:
+        logging.exception("Failed to ingest ... try again later?")
+
+
+@app.route("/elevate", methods=['POST'])
+def ingest_elevate():
+
+    try:
+
+        # request arguments
+        json = request.get_json(force=True) # https://stackoverflow.com/questions/53216177/http-triggering-cloud-function-with-cloud-scheduler/60615210#60615210
+        download_strategy = escape(json['download_strategy']) if 'download_strategy' in json else 'ftps'
+        tables = escape(json['tables']) if 'tables' in json else 'all'
+        env = escape(json['env']) if 'env' in json else 'prod'
+        extract_date = escape(json['extract_date']) if 'extract_date' in json else date.today().strftime("%Y%m%d")
+
+        overrides = {
+            'download_strategy': download_strategy,
+            'tables': tables,
+            'env': env
+        }
+
+        config = dict_from_yaml('elevate/config.yaml')
+        ingest_elevate = IngestElevate(config, extract_date, overrides)
+        ingest_elevate.run()
         ok = 'Ingested successfully'
         logging.info(ok)
         return ok
