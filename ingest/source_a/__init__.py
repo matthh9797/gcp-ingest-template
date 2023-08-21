1407d601e1a55a4ef4e021a67fb6ff68633240e1
matthh9797jetty <matthew.henderson@jettytechnologies.com>
2023-08-21 14:31:04 +0100

    removed source a

--- ingest/source_a/__init__.py
+++
@@ -1,7 +0,0 @@
-from ingest import Ingest
-
-## @TODO for developer: Define child classes of base Ingest class 
-## and override methods if required. Rename child class to match the source.
-
-class IngestA(Ingest):
-    pass
--- ingest/source_a/transform.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-def transform_table_a(table_obj: dict) -> pd.DataFrame:
-    """
-    Function to transform table a from source a
-    @table_obj object containing table specific configuration 
-    """
-    pass
