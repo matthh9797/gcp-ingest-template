d42a9adf4a69debd3d31940e36e4279a23a818d9
matthh9797jetty <matthew.henderson@jettytechnologies.com>
2023-08-21 13:48:38 +0100

    moved setup scripts

--- ingest/source_a/setup/04_call_cr.sh
+++ ingest/source_a/setup/04_call_cr.sh
@@ -6,11 +6,13 @@ NAME={{REPLACE}} # e.g. ingest-yfinance-daily
 PROJECT_ID=$(gcloud config get-value project)
 
 URL=$(gcloud run services describe {{REPLACE}} --format 'value(status.url)')
-echo $URL
+FUNCTION=${URL}/{{REPLACE}} # with name of endpoint
+echo $FUNCTION
 
-# Feb 2015
+# For testing downloading directly from storage bucket. Either replace with 'winscp' or send no message to download from WinSCP
 echo {\"env\":\"prod\"\} > /tmp/message
 
-curl -k -X POST $URL \
+
+curl -k -X POST $FUNCTION \
    -H "Authorization: Bearer $(gcloud auth print-identity-token)" \
    -H "Content-Type:application/json" --data-binary @/tmp/message
