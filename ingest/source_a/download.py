3c9d9e36ebd211f4d12761f6327642efb22622d8
matthh9797jetty <matthew.henderson@jettytechnologies.com>
2023-08-21 14:00:24 +0100

    added download and transform subfolders

---
+++ ingest/source_a/download.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+def download_table_a(table_obj: dict) -> pd.DataFrame:
+    """
+    Function to download table a from source a
+    @table_obj object containing table specific configuration 
+    """
+    pass
